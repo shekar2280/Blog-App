c318e82baf7d4a4feeaf16a791d4c9e29da18a18
Soma Shekar <Somashekar528234@gmail.com>
2024-11-02 22:10:52 +0530

    Changes in auth and connect in utils folder

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,3 @@
-import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth, { getServerSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -10,10 +9,6 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 };
